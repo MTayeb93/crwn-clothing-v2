a7ea2c228078e261a7faf19285742eec4df2d417
MTayeb93 <m.tayeb993@gmail.com>
2023-03-26 12:31:50 +0300

    finish working on sign in component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -24,18 +24,35 @@ const firebaseApp = initializeApp(firebaseConfig)
 const provider = new GoogleAuthProvider()
 
 provider.setCustomParameters({
- prompt: 'select_account'
+  prompt: 'select_account',
 })
 
+//setting up the popup sign in method
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 
+//creating the database
 export const db = getFirestore()
 
+//create users collection
 export const createUserDocumentFromAuth = async (userAuth) => {
- const userDocRef = doc(db, 'users', userAuth.uid)
- console.log(userDocRef);
+  const userDocRef = doc(db, 'users', userAuth.uid)
 
- const userSnapshot = await getDoc(userDocRef)
- console.log(userSnapshot);
+  const userSnapshot = await getDoc(userDocRef)
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      })
+    } catch (error) {
+      console.log('error creating the user', error.message)
+    }
+  }
+  return userDocRef
 }
